826d09d08846e134f98d3ee3f86ca3ae6d83b319
YuanXi <xjr815226031@outlook.com>
2024-05-15 06:40:59 -0700

    initialise feature: create several files, add images, see detail:
    htmlTemplate.ejs:
    1.create
    
    index.html:
    1.delete
    
    index.js:
    1.add static files
    
    style.css
    1.add global 样式
    
    images/:
    1. add 3 kind of logo
    
    navbar.ejs:
    1. create
    2. initialise
    
    resetPassword.ejs
    1. create

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from "express"
-import connectDB from "./db.js";
-import path from "path"
-import session from "express-session"
 import MongoStore from "connect-mongo"
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import session from "express-session"
+import {fileURLToPath} from 'url';
+import connectDB from "./db.js";
+import {dirname} from 'path';
+import express from "express"
 import bcrypt from 'bcrypt';
+import path from "path"
 //import User from "./model/User.js"; // Import the User model
 
 const __filename = fileURLToPath(import.meta.url);
@@ -22,94 +22,102 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Static files
 app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended: true }));
+app.use("/styles", express.static(path.resolve(__dirname, "./public/styles")));
+app.use("/images", express.static(path.resolve(__dirname + "./views/images")));
+
+app.use(express.urlencoded({extended: true}));
 app.use(
     session({
-      secret: process.env.NODE_SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI,
-      }),
+        secret: process.env.NODE_SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI,
+        }),
     })
 );
 
 
 app.get("/", (req, res) => {
-  //register();
-  //login()
-  res.render("index");
+    //register();
+    //login()
+    res.render("index");
 });
 
 app.get('/signup', (req, res) => {
-  res.render('signup');
+    res.render('signup');
+});
+
+app.get('/resetPassword', (req, res) => {
+    res.render('resetPassword');
 });
 
 app.post('/submitUser', async (req, res) => {
 
 
-  try {
+    try {
 
-    const user_name = req.body.user_name
-    const first_name = req.body.first_name
-    const last_name = req.body.last_name
-    const email = req.body.email
+        const user_name = req.body.user_name
+        const first_name = req.body.first_name
+        const last_name = req.body.last_name
+        const email = req.body.email
 
-    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-
-
-    req.session.userData = {
-      user_name: user_name,
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: hashedPassword
-    };
-
-    console.log("hi");
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
 
-    res.redirect('/additional-info');
-  } catch (error) {
-    console.error('Error hashing password:', error);
-    res.status(500).send('Internal Server Error');
-  }
+        req.session.userData = {
+            user_name: user_name,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: hashedPassword
+        };
+
+        console.log("hi");
+
+
+        res.redirect('/additional-info');
+    } catch (error) {
+        console.error('Error hashing password:', error);
+        res.status(500).send('Internal Server Error');
+    }
 
 
 });
 
 
-app.get("/additional-info", (req,res) => {
-  res.render("additional-info");
+app.get("/additional-info", (req, res) => {
+    res.render("additional-info");
 })
 
 app.post("/submitAdditionalInfo", (req, res) => {
 
-  var weight = req.body.weight;
-  var height = req.body.height;
-  var workoutLevel = req.body.workoutLevel;
+    var weight = req.body.weight;
+    var height = req.body.height;
+    var workoutLevel = req.body.workoutLevel;
 
-  req.session.userData = {
-    ...req.session.userData,
-    weight: weight,
-    height: height,
-    workoutLevel: workoutLevel
-  }
+    req.session.userData = {
+        ...req.session.userData,
+        weight: weight,
+        height: height,
+        workoutLevel: workoutLevel
+    }
 
-  res.redirect("/test");
+    res.redirect("/test");
 })
 
 //test page after user enters all their info
 app.get("/test", (req, res) => {
-  // Access user data from the session
-  const userData = req.session.userData;
+    // Access user data from the session
+    const userData = req.session.userData;
 
-  // Render a view with the user data
-  res.render("test", { userData: userData });
-  console.log(userData);
+    // Render a view with the user data
+    res.render("test", {userData: userData});
+    console.log(userData);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started: http://localhost:${PORT}`);
+    console.log(`Server started: http://localhost:${PORT}`);
 });
