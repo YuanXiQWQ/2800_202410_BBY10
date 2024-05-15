5de336acbc5c57c7aba3e6ad40a4181a9772590d
YuanXi <xjr815226031@outlook.com>
2024-05-15 04:38:59 -0700

    change requirement and unique in user.js, initialise index.js
    copied index.js from matt's fearure/createUser

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import session from "express-session"
 import MongoStore from "connect-mongo"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import bcrypt from 'bcrypt';
+//import User from "./model/User.js"; // Import the User model
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const saltRounds = 10;
 
 connectDB();
 
@@ -19,26 +23,93 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(__dirname + "/public"));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
-  session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
-    }),
-  })
+    session({
+      secret: process.env.NODE_SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+      }),
+    })
 );
 
 
 app.get("/", (req, res) => {
-  register();
-  login()
+  //register();
+  //login()
+  res.render("index");
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.post('/submitUser', async (req, res) => {
+
+
+  try {
+
+    const user_name = req.body.user_name
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+    const email = req.body.email
+
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+
+    req.session.userData = {
+      user_name: user_name,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: hashedPassword
+    };
+
+    console.log("hi");
+
+
+    res.redirect('/additional-info');
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    res.status(500).send('Internal Server Error');
+  }
+
+
+});
+
+
+app.get("/additional-info", (req,res) => {
+  res.render("additional-info");
+})
+
+app.post("/submitAdditionalInfo", (req, res) => {
+
+  var weight = req.body.weight;
+  var height = req.body.height;
+  var workoutLevel = req.body.workoutLevel;
+
+  req.session.userData = {
+    ...req.session.userData,
+    weight: weight,
+    height: height,
+    workoutLevel: workoutLevel
+  }
+
+  res.redirect("/test");
+})
+
+//test page after user enters all their info
+app.get("/test", (req, res) => {
+  // Access user data from the session
+  const userData = req.session.userData;
+
+  // Render a view with the user data
+  res.render("test", { userData: userData });
+  console.log(userData);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started: http://localhost:${PORT}`);
 });
--- model/User.js
+++ model/User.js
@@ -1,56 +1,57 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        firstName: {
+            type: String,
+            required: false,
+        },
+        lastName: {
+            type: String,
+            required: false,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        height: {
+            type: Number,
+            required: false,
+        },
+        weight: {
+            type: Number,
+            required: false,
+        },
+        goal: {
+            type: String,
+            required: false,
+        },
+        time: {
+            type: [Number],
+            required: false,
+            enum: [0, 1, 2, 3, 4, 5, 6],
+        },
+        birthday: {
+            type: Date,
+            required: false,
+        },
+        fitnessLevel: {
+            type: String,
+            required: false,
+            enum: ["beginner", "intermediate", "advanced"],
+        },
     },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    height: {
-      type: Number,
-      required: true,
-    },
-    weight: {
-      type: Number,
-      required: true,
-    },
-    goal: {
-      type: String,
-      required: true,
-    },
-    time: {
-      type: [Number],
-      required: true,
-      enum: [0, 1, 2, 3, 4, 5, 6],
-    },
-    birthday: {
-      type: Date,
-      required: true,
-    },
-    fitnessLevel: {
-      type: String,
-      required: true,
-      enum: ["beginner", "intermediate", "advanced"],
-    },
-  },
-  { timestamps: true }
+    {timestamps: true}
 );
 
 const User = mongoose.model("users", userSchema);
