00a1899c52e83414a32a829adca5355d1b5c2653
YuanXi <xjr815226031@outlook.com>
2024-05-15 04:43:23 -0700

    db.js: delete deprecated option

--- db.js
+++ db.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 export default async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("Error connecting to MongoDB:", error);
