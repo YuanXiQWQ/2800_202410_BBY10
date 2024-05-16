729c20a2154fb8c393230d48db3ba1006e5b3ff4
YuanXi <xjr815226031@outlook.com>
2024-05-16 05:49:03 -0700

    finish profile page and its related functions

--- index.js
+++ index.js
@@ -51,12 +51,6 @@ app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.get('/changePassword', (req, res) => {
-    res.render('changePassword');
-});
-
-app.post('/change-password', changePassword);
-
 app.post('/submitUser', async (req, res) => {
     try {
         await register(req, res);
