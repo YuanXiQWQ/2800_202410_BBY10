446576fea575d5ecd0bc95421c239ce3091410c7
amal allaham <amalallahham20@gmail.com>
2024-05-17 12:38:03 -0700

    quick fix

--- controller/login.js
+++ controller/login.js
@@ -1,21 +1,24 @@
-import {findByUsername, validatePassword} from './auth.js';
+import { findByUsername, validatePassword } from "./auth.js";
 
 export async function logIn(req, res) {
-    const {username, password} = req.body;
-    try {
-        const user = await findByUsername(username);
-        if (!user) {
-            return res.status(404).json({message:'User not found', success: false});
-        }
-        const isMatch = await validatePassword(user, password);
-        if (!isMatch) {
-            return res.status(400).json({message:'Invalid password', success: false});
-        }
-        req.session.userData = user;
-        res.status(200).json({success:true});
+  const { username, password } = req.body;
+  try {
+    const user = await findByUsername(username);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
     }
-    catch (error) {
-        console.error('Error logging in:', error);
-        res.status(500).json({message:'Internal Server Error', success: false});
+    const isMatch = await validatePassword(user, password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid password", success: false });
     }
+    req.session.userData = user;
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ message: "Internal Server Error", success: false });
+  }
 }
