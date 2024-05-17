bf7f63e92b8c3d663a5488707964d97defb5f7f4
amal allaham <amalallahham20@gmail.com>
2024-05-17 15:24:19 -0700

    additional info validation

--- controller/auth.js
+++ controller/auth.js
@@ -1,27 +1,43 @@
-import bcrypt from 'bcrypt';
-import { User } from '../model/user.js';
+import bcrypt from "bcrypt";
+import { User } from "../model/user.js";
 import crypto from "crypto";
 import path from "path";
 import { GridFSBucket } from "mongodb";
 import mongoose from "mongoose";
 import Joi from "joi";
 
-
 const schemaSignin = Joi.object({
-    email: Joi.string().email().required(),
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().max(20).required(),
-    firstName: Joi.string().alphanum().min(3).max(30).required(),
-    lastName: Joi.string().alphanum().min(3).max(30).required(),
-    birthday: Joi.date().iso().max('now').required(),
-  });
-  
-  const schemaLogin = Joi.object({
-    email: Joi.string().email(),
-    password: Joi.string().max(20).required(),
-  });
-  
-  
+  email: Joi.string().email().required(),
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().max(20).required(),
+  firstName: Joi.string().alphanum().min(3).max(30).required(),
+  lastName: Joi.string().alphanum().min(3).max(30).required(),
+  birthday: Joi.date().iso().max("now").required(),
+});
+
+const schemaInfo = Joi.object({
+  weight: Joi.number().positive().required(),
+
+  height: Joi.number().positive().required(),
+
+  time: Joi.number()
+    .valid(0, 1, 2, 3, 4, 5, 6) // Enumerating valid options for 'time'
+    .required(),
+
+  goal: Joi.string()
+    .min(3) // Minimum length of the goal text
+    .max(200) // Maximum length
+    .required(),
+
+  fitnessLevel: Joi.string()
+    .valid("beginner", "intermediate", "advanced") // Enumerating valid options for 'fitnessLevel'
+    .required(),
+});
+
+const schemaLogin = Joi.object({
+  email: Joi.string().email(),
+  password: Joi.string().max(20).required(),
+});
 
 /**
  * Function to find a user in the database by username.
@@ -30,7 +46,7 @@ const schemaSignin = Joi.object({
  * @return {Promise<User|null>} A promise that resolves to the user document if found, or null if not found.
  */
 export async function findByUsername(username) {
-    return await User.findOne({ username });
+  return await User.findOne({ username });
 }
 
 /**
@@ -51,55 +67,58 @@ export async function validatePassword(user, password) {
  * @param {Response} res - Express response object
  */
 export async function register(req, res) {
-    const saltRounds = 10;
-    const { username, firstName, lastName, email, birthday, password } = req.body;
-  
-    const validationResult = schemaSignin.validate({ username, firstName, lastName, email, birthday, password });
-  
-    if (validationResult.error) {
-      return res
-        .status(400)
-        .render("validationError", {
-          error: validationResult.error.details[0].message, 
-          link: "signup",
-          linkImage: "images/validation-error.jpg"
-        });
-    }
-  
-    try {
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res
-          .status(409)
-          .render("validationError", {
-            error: "User already exists with this email.",
-            link: "signup",
-            linkImage: "validation-error.jpg",
-          });
-      }
-  
-      bcrypt.hash(password, saltRounds)
-        .then((hashedPassword) => {
-          req.session.userData = {
-            username,
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-            birthday,
-          };
-          res.redirect("/additional-info");
-        })
-        .catch((error) => {
-          console.error("Error hashing password:", error);
-          res.status(500).send("Internal Server Error");
-        });
-    } catch (error) {
-      console.error(error, 'error');
-      return res.status(500).json({ message: "Internal server error" });
-    }
+  const saltRounds = 10;
+  const { username, firstName, lastName, email, birthday, password } = req.body;
+
+  const validationResult = schemaSignin.validate({
+    username,
+    firstName,
+    lastName,
+    email,
+    birthday,
+    password,
+  });
+
+  if (validationResult.error) {
+    return res.status(400).render("validationError", {
+      error: validationResult.error.details[0].message,
+      link: "signup",
+      linkImage: "images/validation-error.jpg",
+    });
   }
-  
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).render("validationError", {
+        error: "User already exists with this email.",
+        link: "signup",
+        linkImage: "validation-error.jpg",
+      });
+    }
+
+    bcrypt
+      .hash(password, saltRounds)
+      .then((hashedPassword) => {
+        req.session.userData = {
+          username,
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+          birthday,
+        };
+        res.redirect("/additional-info");
+      })
+      .catch((error) => {
+        console.error("Error hashing password:", error);
+        res.status(500).send("Internal Server Error");
+      });
+  } catch (error) {
+    console.error(error, "error");
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 /**
  * Function to add additional user information.
@@ -109,16 +128,29 @@ export async function register(req, res) {
  */
 export async function AdditionalUserInfo(req, res) {
 
-    // const { error } = additionalInfoSchema.validate(req.body);
-    // if (error) {
-    //     console.error('Error validating additional user info:', error);
-    //     res.redirect("/additional-info");
-    //     return;
-    // }
+  const { weight, height, time, goal, fitnessLevel } = req.body;
+
+  const validationResult2 = schemaInfo.validate({
+    weight, height, time, goal, fitnessLevel
+  });
+
+  if (validationResult2.error) {
+    return res.status(400).render("validationError", {
+      error: validationResult2.error.details[0].message,
+      link: "additional-info",
+      linkImage: "images/validation-error.jpg",
+    });
+  }
 
 
-    const { weight, height, time, goal, fitnessLevel } = req.body;
-    const { username, firstName, lastName, email, birthday, password: hashedPassword } = req.session.userData;
+  const {
+    username,
+    firstName,
+    lastName,
+    email,
+    birthday,
+    password: hashedPassword,
+  } = req.session.userData;
 
   req.session.userData = {
     ...req.session.userData,
@@ -128,6 +160,7 @@ export async function AdditionalUserInfo(req, res) {
     goal,
     fitnessLevel,
   };
+
 
   const newUser = new User({
     username,
